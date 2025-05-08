b99e584042d229649e1441697a58b4d77ad9a2bd
wangyuching <5b1g0023@stust.edu.tw>
2025-05-08 11:06:24 +0800

    0508

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import reactLogo from "./assets/react.svg";
 import MySlider from "./component/MySlider";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,19 +10,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" title="Vite">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" title="React">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>123456789陳小明</h1>
-      <h2>我的第一個元件：Slider</h2>
+      <h1>20250508</h1>
+      <h2>我的第一個元件 : Slider</h2>
       <div>
-        <p>R:<MySlider /></p>
-        <p>G:<MySlider /></p>
-        <p>B:<MySlider /></p>
+        <p>R : <MySlider /></p>
+        <p>G : <MySlider /></p>
+        <p>B : <MySlider /></p>
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
